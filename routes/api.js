5f22789f2b0d6d618e2dd80397c6267355fa7712
hardikk2002 <hardikk2002@gmail.com>
2021-08-30 13:29:59 +0530

    APIs setup...

---
+++ routes/api.js
@@ -0,0 +1,72 @@
+const axios = require("axios");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const apiKeyClist = `username=${process.env.USER_NAME}&api_key=${process.env.API_KEY_CLIST}`;
+const apiKeyNews = `apiKey=${process.env.API_KEY_NEWS}`;
+
+app.post("/contest", (req, res) => {
+  var resource = req.body.resource;
+  // console.log(req.body.resource);
+  axios
+    .get(
+      `https://clist.by:443/api/v2/json/contest/?limit=30&resource=${resource}&order_by=-start&${apiKeyClist}`
+    )
+    .then((response) => {
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      res.status(500).json("error");
+    });
+});
+
+app.get("/news-apple", (req, res) => {
+  var currentTime = new Date().toJSON().slice(0, 10);
+  axios
+    .get(
+      `https://newsapi.org/v2/everything?q=apple&from=${currentTime}&sortBy=popularity&${apiKeyNews}`
+    )
+    .then((response) => {
+      // console.log(response.data.articles.slice(0, 10));
+      res.status(200).json(response.data.articles.slice(0, 10));
+    })
+    .catch((error) => {
+      // console.log(error);
+      res.status(500).json("error");
+    });
+});
+
+app.get("/news-tesla", (req, res) => {
+  var currentTime = new Date().toJSON().slice(0, 10);
+  axios
+    .get(
+      `https://newsapi.org/v2/everything?q=tesla&from=${currentTime}&sortBy=popularity&${apiKeyNews}`
+    )
+    .then((response) => {
+      // console.log(response.data.articles.slice(0, 10));
+      res.status(200).json(response.data.articles.slice(0, 10));
+    })
+    .catch((error) => {
+      // console.log(error);
+      res.status(500).json("error");
+    });
+});
+app.post("/news-keyword", (req, res) => {
+  var resource = req.body.resource;
+  var currentTime = new Date().toJSON().slice(0, 10);
+  // console.log(resource);
+  axios
+    .get(
+      `https://newsapi.org/v2/everything?q=${resource}&from=${currentTime}&sortBy=popularity&${apiKeyNews}`
+    )
+    .then((response) => {
+      res.status(200).json(response.data.articles.slice(0, 10));
+    })
+    .catch((error) => {
+      // console.log(error);
+      res.status(500).json("error");
+    });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const axios = require("axios");
-const { response } = require("express");
+
 const app = express();
-require("dotenv").config();
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -17,70 +15,7 @@ app.use(function (req, res, next) {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const apiKeyClist = `username=${process.env.USER_NAME}&api_key=${process.env.API_KEY_CLIST}`;
-const apiKeyNews = `apiKey=${process.env.API_KEY_NEWS}`;
-app.post("/contest", (req, res) => {
-  var resource = req.body.resource;
-  // console.log(req.body.resource);
-  axios
-    .get(
-      `https://clist.by:443/api/v2/json/contest/?limit=30&resource=${resource}&order_by=-start&${apiKeyClist}`
-    )
-    .then((response) => {
-      res.status(200).json(response.data);
-    })
-    .catch((error) => {
-      res.status(500).json("Server Error, try again latter");
-    });
-});
-
-app.get("/news-apple", (req, res) => {
-  var currentTime = new Date().toJSON().slice(0, 10);
-  axios
-    .get(
-      `https://newsapi.org/v2/everything?q=apple&from=${currentTime}&sortBy=popularity&${apiKeyNews}`
-    )
-    .then((response) => {
-      // console.log(response.data.articles.slice(0, 10));
-      res.status(200).json(response.data.articles.slice(0, 10));
-    })
-    .catch((error) => {
-      // console.log(error);
-      res.status(500).json("Server Error, try again latter");
-    });
-});
-app.get("/news-tesla", (req, res) => {
-  var currentTime = new Date().toJSON().slice(0, 10);
-  axios
-    .get(
-      `https://newsapi.org/v2/everything?q=tesla&from=${currentTime}&sortBy=popularity&${apiKeyNews}`
-    )
-    .then((response) => {
-      // console.log(response.data.articles.slice(0, 10));
-      res.status(200).json(response.data.articles.slice(0, 10));
-    })
-    .catch((error) => {
-      // console.log(error);
-      res.status(500).json("Server Error, try again latter");
-    });
-});
-app.post("/news-keyword", (req, res) => {
-  var resource = req.body.resource;
-  var currentTime = new Date().toJSON().slice(0, 10);
-  console.log(resource);
-  axios
-    .get(
-      `https://newsapi.org/v2/everything?q=${resource}&from=${currentTime}&sortBy=popularity&${apiKeyNews}`
-    )
-    .then((response) => {
-      res.status(200).json(response.data.articles.slice(0, 10));
-    })
-    .catch((error) => {
-      // console.log(error);
-      res.status(500).json("Server Error, try again latter");
-    });
-});
-
+app.use("/api", require("./routes/api"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
