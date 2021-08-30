0e8a1bd6824dcbac0db843a5b50919b91c7012a7
hardikk2002 <hardikk2002@gmail.com>
2021-08-30 22:54:38 +0530

    Socket io chat serup

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require("express");
 const bodyParser = require("body-parser");
-
-
-const app = express();
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -16,8 +19,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api", require("./routes/api"));
+app.get("/", (req, res) => {
+  res.send("Sahi ha biddu");
+});
 
-const port = process.env.PORT || 5000;
-app.listen(port, () =>
-  console.log(`App listening at http://localhost:${port}`)
+io.on("connection", (socket) => {
+  // console.log("connection made successfully");
+  socket.on("message", (payload) => {
+    // console.log("Message received on server: ", payload);
+    io.emit("message", payload);
+  });
+});
+
+server.listen(process.env.PORT || 4000, () =>
+  console.log(`Server has started on 4000.`)
 );
